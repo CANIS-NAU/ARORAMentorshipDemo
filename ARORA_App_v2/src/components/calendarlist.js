7c65cc6a254164c0cd92bf1081da4f9863c4c740
swhanson <77860949+swhanson@users.noreply.github.com>
2022-02-20 14:26:35 -0700

    CalendarList

---
+++ ARORA_App_v2/src/components/calendarlist.js
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import {styles} from '../stylesheet';
+import {NavigationBar} from './navigationbar';
+import { StyleSheet, View, Text, Button, Pressable, Image, TextInput, Divider } from 'react-native';
+
+
+export default function CalendarList( {navigation} ) {
+  const events = [{
+                    date: '05/10/2022',
+                    time: '12:30 PM',
+                    desc: 'Meeting with John Smith'
+                  },
+                  {
+                    date: '05/12/2022',
+                    time: '6:00 PM',
+                    desc: 'Jane Doe\'s birthday'
+                  }
+                  ]
+
+  return (<View style={styles.homescreenmenteelist}>
+    {events.map(event =>
+                  (<View style={styles.homescreenmentee}>
+                      <Text>{event.date}</Text>
+                      <Text>{event.time}</Text>
+                      <Text>{event.desc}</Text>
+                    </View>))}
+    </View>)
+}
--- ARORA_App_v2/src/screens/calendarscreen.js
+++ ARORA_App_v2/src/screens/calendarscreen.js
@@ -2,15 +2,14 @@ import * as React from 'react';
 import {styles} from '../stylesheet';
 import { StyleSheet, View, Text, Button, Pressable, Image, TextInput } from 'react-native';
 
-export function CalendarScreen( {navigation} )
+export function CalendarScreen( {route, navigation} )
 {
   return (
     <View style={styles.screen}>
 
-      <View style={styles.screencontent}>
-        <Text>Google Calendar</Text>
-      </View>
+      <View style={styles.moodreportlist}>
 
+      </View>
     </View>
   );
 }
