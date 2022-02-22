732588281d1e00066db622ae4b0a9e7201bae874
Shyguy380 <70126498+Shyguy380@users.noreply.github.com>
2022-02-22 09:07:53 -0700

    Screen to add new mentors.
    Need to incorporate database functionality.

---
+++ ARORA_App_v2/src/screens/creatementorscreen.js
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import {styles} from '../stylesheet';
+import { StyleSheet, View, Text, Button, Pressable, Image, TextInput, Divider } from 'react-native';
+
+export function CreateMentorScreen({ navigation }) {
+  const [accessCode, codeText] = React.useState('');
+
+  return (
+    <View style={styles.screen}>
+      <View style={styles.screencontent}>
+        <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
+
+          {/* Supervisor only needs to make new blanks with a set access code*/}
+          <TextInput style={styles.logininput} 
+                     placeholder = "Access Code"
+                     onChangeText = {accessCode => codeText(accessCode)} 
+                     defaultValue = {accessCode}/>
+
+        <Pressable style={styles.loginoption}
+                              onPress={() => navigation.navigate('Profile')}>
+              <Text style={styles.loginoptiontext}>Return to Login</Text>
+        </Pressable>
+
+        {/* Need to add what happens to other data, preferebly a record in database */}
+        <Pressable style={styles.loginbutton} 
+                     onPress={(accessCode != null) ? () => navigation.navigate('Profile') : null}>
+              <Text style={styles.loginbuttontext}> Submit </Text>
+          </Pressable>
+
+        </View>
+      </View>
+    </View>
+  );
+}
