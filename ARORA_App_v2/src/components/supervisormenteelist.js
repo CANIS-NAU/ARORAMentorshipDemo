784c1641b098cc802159e1a1effbe66270a1135d
ashlemia <82060474+ashlemia@users.noreply.github.com>
2022-03-29 01:05:59 -0700

    Resolving Conflicts Push

--- ARORA_App_v2/src/components/supervisormenteelist.js
+++ ARORA_App_v2/src/components/supervisormenteelist.js
@@ -49,6 +49,10 @@ export default function SupervisorMenteeList( {navigation} ) {
 
     const [refreshing, setRefreshing] = React.useState(false);
 
+    const wait = (timeout) => {
+        return new Promise(resolve => setTimeout(resolve, timeout));
+      }
+
     const onRefresh = React.useCallback(() => {
         setRefreshing(true);
         wait(2000).then(() => setRefreshing(false));
